8aecb7c99f3b61dc7f93c00f0e23cb07c0861a91
josegit94 <joserevolver50@gmail.com>
2022-03-10 10:39:02 -0800

    build a puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 *the main put function print programming is like building a multilingual puzzle
 *Return: always0 (success)
 */
-int main(void)
+int main(void) /*main function*/
 {
-puts("\"Programming is like a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
