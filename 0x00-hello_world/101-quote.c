e2c9127951cffcfa199c33c457f7f3e1caf02669
josegit94 <joserevolver50@gmail.com>
2022-03-10 12:57:45 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
 	return (1);
 }
