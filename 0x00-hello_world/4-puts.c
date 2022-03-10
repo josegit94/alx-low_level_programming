b95ef92b438fe315e55045f1a5a5ce4b77929a0d
josegit94 <joserevolver50@gmail.com>
2022-03-10 10:14:07 -0800

    programming is like am ultilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*the main put function 
+ *print programming is like building a multilingual puzzle
+ * return 0 
+ */
+
+int main
+{
+puts("\"Programming is like a multilingual puzzle"); 
+return (0);
+}
