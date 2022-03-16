6070e0c205a763d23d4d2cf5f81902dcfb47b82b
josegit94 <joserevolver50@gmail.com>
2022-03-15 17:37:46 -0700

    more fiboncci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i;
-	long int fibonacci[98];
+	long int fibonacci[100];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
@@ -18,7 +18,7 @@ int main(void)
 	for (i = 2; i < 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 97)
+		if (i == 98)
 			printf("%ld\n", fibonacci[i]);
 		else
 			printf("%ld, ", fibonacci[i]);
