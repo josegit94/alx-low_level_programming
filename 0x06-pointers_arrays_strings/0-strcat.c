de1a37e9de255694dbfcaa96aa755a95f9b7663d
josegit94 <joserevolver50@gmail.com>
2022-03-25 02:00:35 -0700

    concantenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * *_strcat - concatenates two strings
  * @dest: pointer destination
  * @src: pointer source
  * Return: void
- */
+*/
 
 char *_strcat(char *dest, char *src)
 {
