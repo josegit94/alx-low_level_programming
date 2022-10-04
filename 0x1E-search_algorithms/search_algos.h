e0e5680434248e6abbfbb82e288b53014e1f566f
josegit94 <joserevolver50@gmail.com>
2022-10-04 15:06:13 -0700

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,10 +6,11 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-int jump_search(int *array, size_t size, int value);
+
 #endif
